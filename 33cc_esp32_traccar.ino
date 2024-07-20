291bfcd29dd4c0c38d37eb245f773ce1df6b4a19
Bart Matterne <u278824@sunwfrk.net>
2024-07-20 20:35:30 +0200

    feat: HTTP POST over WiFi or GPRS

--- 33cc_esp32_traccar.ino
+++ 33cc_esp32_traccar.ino
@@ -2,34 +2,58 @@
 #define TINY_GSM_RX_BUFFER 1024 // Set RX buffer to 1Kb
 #define SerialAT Serial1
 
+#define USE_WIFI false
+#define USE_GPRS true
+
+#if USE_WIFI
 #include <WiFi.h>
-#include <WiFiClientSecure.h>
-#include "time.h"
+#endif
+
+#include "Time.h"
 #include <TinyGsmClient.h>
+#include <ArduinoHttpClient.h>
 #include "utilities.h"
 #include "arduino_secrets.h"
+#include <ESP32Time.h>
 
+// Date/Time Config
+ESP32Time rtc(3600);
 
+TinyGsm modem(SerialAT);
+#if USE_GPRS
+// Mobile config
+const char gprsApn[]  = SECRET_GPRS_APN;
+const char gprsUser[] = SECRET_GPRS_USER;
+const char gprsPass[] = SECRET_GPRS_PASS;
+
+// Initialize TinyGSM
+TinyGsmClient gprs(modem);
+#endif
+
+#if USE_WIFI
 // WiFi config
-const char* ssid = SECRET_WIFI_SSID;
-const char* password = SECRET_WIFI_PASSWORD;
+WiFiClient wifi;
+const char* wifiSSID = SECRET_WIFI_SSID;
+const char* wifiPass = SECRET_WIFI_PASS;
+#endif
 
-// HTTPS connection config
-const char* host = SECRET_TRACCAR_HOST;
-const int httpsPort = 443;
-const char* resource = "/";
+#if USE_WIFI
+HttpClient client = HttpClient(wifi, SECRET_TRACCAR_HOST, SECRET_TRACCAR_PORT);
+#endif
+
+#if USE_GPRS
+HttpClient client = HttpClient(gprs, SECRET_TRACCAR_HOST, SECRET_TRACCAR_PORT);
+#endif
+
 // Traccar ID
 String id = SECRET_TRACCAR_ID;
 
-// Accuracy seettings
+// Accuracy settings for GPS reporting
 float degreemargin = 0.10;
 float accuracymargin = 4.00;
 // should be kept at the initial value of 0.00
 float previouslat = 0.00;
 float previouslon = 0.00;
-
-// NTP server to request epoch time
-const char* ntpServer = "be.pool.ntp.org";
 
 // Variable to save current epoch time
 unsigned long epochTime;
@@ -46,14 +70,12 @@ unsigned long getTime() {
   return now;
 }
 
-TinyGsm modem(SerialAT);
-
 // Function to validate coordinates and accuracy
 void verify_data(float lat, float lon, float speed, float alt, float accuracy) {
   int sendit = 0;
   Serial.print("Sendit value is set to: ");
   Serial.println(sendit);
-  // check if we are still using the initial lat/lon previous values
+  // check if we are still using the initial lat/lon values
   if (( previouslat == 0.00f ) or ( previouslon == 0.00f )) {
     Serial.println("Using initial GPS coordinates");
     Serial.println("Checking Accuracy..");
@@ -69,10 +91,10 @@ void verify_data(float lat, float lon, float speed, float alt, float accuracy) {
       previouslon = lon;
       Serial.println("Accuracy OK");
     } else {
-      Serial.println("Accuracy Not OK");
+      Serial.println("Accuracy is not OK");
     }
+  // We are not using initial lat/lon values
   } else {
-    Serial.println("Not using initial GPS coordinates");
     Serial.println("Checking Accuracy");
     Serial.print("Accuracy is: ");
     Serial.println(accuracy);
@@ -94,7 +116,7 @@ void verify_data(float lat, float lon, float speed, float alt, float accuracy) {
       Serial.print("lon low margin is: ");
       Serial.println(lonmarginl,6);
       if ((lat > latmarginl) && (lat < latmarginh) && (lon > lonmarginl) && (lon < lonmarginh)) {
-        Serial.println("Coordinates between margins");
+        Serial.println("Coordinates are between margins");
         sendit = 1;
         previouslat = lat;
         previouslon = lon;
@@ -110,59 +132,31 @@ void verify_data(float lat, float lon, float speed, float alt, float accuracy) {
 // Function to send GPS data to the server
 void send_data(float lat, float lon, float speed, float alt, float accuracy) {
 
-  // Use WiFiClientSecure class to create a TLS connection
-  WiFiClientSecure client;
-  client.setInsecure();  // Set to use an insecure connection
-
-  Serial.print("Connecting to ");
-  Serial.println(host);
-
-  if (!client.connect(host, httpsPort)) {
-    Serial.println("Connection failed");
-    delay(10000); // Wait for 10 seconds before retrying
-    return;
-  }
-
-  // Make a HTTP POST request
   epochTime = getTime();
-  String postData = "id=" + id + "&lat=" + String(lat,6) + "&lon=" + String(lon,6) + "&timestamp=" + String(epochTime) + "&altitude=" + String(alt,6) + "&speed=" + String(speed,6); // Adjust the data to be sent
-  String postRequest =
-      "POST " + String(resource) + " HTTP/1.1\r\n" +
-      "Host: " + String(host) + "\r\n" +
-      "Connection: close\r\n" +
-      "Content-Type: application/x-www-form-urlencoded\r\n" +
-      "Content-Length: " + String(postData.length()) + "\r\n" +
-      "\r\n" +
-      postData;
 
-  Serial.println("Sending POST request...");
+  Serial.println("making POST request");
+  String postData = "id=" + id + "&lat=" + String(lat,6) + "&lon=" + String(lon,6) + "&timestamp=" + String(epochTime) + "&altitude=" + String(alt,6) + "&speed=" + String(speed,6);
 
-  client.print(postRequest);
+  client.beginRequest();
+  client.post("/");
+  client.sendHeader("Content-Type", "application/x-www-form-urlencoded");
+  client.sendHeader("Content-Length", postData.length());
+  client.beginBody();
+  client.print(postData);
+  client.endRequest();
 
-  // Wait for the response
-  while (client.connected()) {
-    String line = client.readStringUntil('\n');
-    if (line == "\r") {
-      Serial.println("Headers received");
-      break;
-    }
-  }
+  // read the status code
+  int statusCode = client.responseStatusCode();
 
-  // Print the response
-  while (client.available()) {
-    Serial.println("Response:");
-    Serial.write(client.read());
-    Serial.print("\n");
-  }
-
-  Serial.println("Closing connection");
-  delay(1000); // Wait for 1 seconds before making the next request
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
 }
 
+#if USE_WIFI
 // Function to set up WiFi
 void setup_wifi() {
   // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
+  WiFi.begin(wifiSSID, wifiPass);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
@@ -170,15 +164,46 @@ void setup_wifi() {
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
 }
+#endif
+
+#if USE_GPRS
+// Function to set up GPRS
+void setup_gprs() {
+  // Unlock your SIM card with a PIN if needed
+  if (SECRET_SIM_PIN && modem.getSimStatus() != 3) {
+    Serial.println("Entering SIM PIN");
+    modem.simUnlock(SECRET_SIM_PIN);
+  }
+
+  // GPRS connection parameters are usually set after network registration
+  Serial.print(F("Connecting to "));
+  Serial.print(gprsApn);
+  while (!modem.gprsConnect(gprsApn, gprsUser, gprsPass)) {
+    Serial.print(".");
+    delay(30000);
+  }
+  Serial.println(" OK");
+}
+#endif
+
+// Variables for GPS
+float lat, lon, speed, alt, accuracy;
+int vsat, usat;
+int year, month, day, hour, minute, second;
 
 // Function to set up time
 void setup_time() {
-  // Configuring time
-  configTime(0, 0, ntpServer);
+  Serial.print(F("Waiting for GPS Data for time config"));
+  while (!modem.getGPS(&lat, &lon, &speed, &alt, &vsat, &usat, &accuracy, &year, &month, &day, &hour, &minute, &second)) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println(" OK");
+  rtc.setTime(second, minute, hour, day, month, year);
 }
 
-// Function to set up the GPS module
-void setup_gps() {
+// Function to set up the Modem
+void setup_modem() {
     Serial.begin(115200); // Set console baud rate
     SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
 
@@ -204,58 +229,74 @@ void setup_gps() {
     pinMode(MODEM_FLIGHT, OUTPUT);
     digitalWrite(MODEM_FLIGHT, HIGH);
 
-    Serial.println("Start modem...");
+    Serial.print("Starting modem");
 
 
     for (int i = 0; i < 3; ++i) {
         while (!modem.testAT(5000)) {
-            Serial.println("Try to start modem...");
+            Serial.print(".");
             pinMode(MODEM_PWRKEY, OUTPUT);
             digitalWrite(MODEM_PWRKEY, HIGH);
             delay(300); //Need delay
             digitalWrite(MODEM_PWRKEY, LOW);
         }
     }
+    Serial.println(" OK");
+}
 
-    Serial.println("Modem Response Started.");
+// Function to set up the GPS module
+void setup_gps() {
 
+    Serial.print("Starting GPS module");
     // Stop GPS Server
     modem.sendAT("+CGPS=0");
     modem.waitResponse(30000);
 
+    Serial.print(".");
     // Configure GNSS support mode
     modem.sendAT("+CGNSSMODE=15,1");
     modem.waitResponse(30000);
 
+    Serial.print(".");
     // Configure NMEA sentence type
     modem.sendAT("+CGPSNMEA=200191");
     modem.waitResponse(30000);
 
+    Serial.print(".");
     // Set NMEA output rate to 1HZ
     modem.sendAT("+CGPSNMEARATE=1");
     modem.waitResponse(30000);
 
+    Serial.print(".");
     // Enable GPS
     modem.sendAT("+CGPS=1");
     modem.waitResponse(30000);
+
+    Serial.println(" OK");
 }
 
 // Setup function
 void setup() {
-  setup_wifi();
+  //delay a little so we can follow
+  delay(3000);
+  setup_modem();
   setup_gps();
   setup_time();
+
+#if USE_WIFI
+  setup_wifi();
+#endif
+
+#if USE_GPRS
+  setup_gprs();
+#endif
+
 }
 
 // Main loop
 void loop() {
-  float lat, lon, speed, alt, accuracy;
-  int vsat, usat;
-  while (1) {
-    if (modem.getGPS(&lat, &lon, &speed, &alt, &vsat, &usat, &accuracy)) {
-      verify_data(lat, lon, speed, alt, accuracy);
-      // send_data(lat, lon, speed, alt, accuracy);
-    }
-    delay(5);
+  if (modem.getGPS(&lat, &lon, &speed, &alt, &vsat, &usat, &accuracy)) {
+    verify_data(lat, lon, speed, alt, accuracy);
   }
+  delay(5000);
 }
